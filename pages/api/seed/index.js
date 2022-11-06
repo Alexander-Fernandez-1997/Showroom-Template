efc110bfde51a1c90d471b75daa98811ab0abc3f
alexander fernandez <alex97l@hotmail.com>
2022-11-06 19:49:18 -0300

    seed all

---
+++ pages/api/seed/index.js
@@ -0,0 +1,19 @@
+import conn from "../../../lib/db";
+
+export default async (req, resp) => {
+  try {
+    //fetch categories, subcategories, clothes, variants in /api/seed
+    const seedFetch = () => {
+      const url = "window.location.origin + '/api/seed/'";
+      const seeds = [categories, subcategories, clothes, variants];
+      const seedFetchs = seeds.map((seed) => {
+        return fetch(url + seed).then((res) => res.json());
+      });
+      return "seed completed";
+    };
+    seedFetch();
+    resp.status(200).json("seed completed");
+  } catch (e) {
+    resp.status(500).json(e.toString());
+  }
+};
