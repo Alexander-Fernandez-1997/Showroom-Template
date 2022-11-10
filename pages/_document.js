08b45e8be7acbc40daaf033f54c5afb905a43a99
alexander fernandez <alex97l@hotmail.com>
2022-11-09 23:03:41 -0300

    fix favicon

--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link rel="icon" type="image/png" href="images/icons/favicon.png" />
+        <link rel="icon" type="image/png" href="/images/icons/favicon.png" />
 
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"
