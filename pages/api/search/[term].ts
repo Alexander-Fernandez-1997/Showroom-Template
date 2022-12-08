ee1f09352fe0154b9b0654a58d346c621d429fe4
alexander fernandez <alex97l@hotmail.com>
2022-12-07 23:03:27 -0300

    fix search page

--- pages/api/search/[term].ts
+++ pages/api/search/[term].ts
@@ -8,9 +8,20 @@ export default async function handler(
   try {
     const { term } = req.query;
     const storeId = req.headers["store-id"];
-    const query = `SELECT * FROM clothes WHERE LOWER(name) LIKE '%${term}%' AND store_id = ${storeId}`;
+    const query = `SELECT * FROM clothes WHERE LOWER(name) LIKE '%${term}%' AND store_id = ${storeId};SELECT * FROM variants WHERE clothes_id IN (SELECT id FROM clothes WHERE LOWER(name) LIKE '%${term}%' AND store_id = ${storeId});`;
     const result = await conn.query(query);
-    res.status(200).json(result.rows);
+
+    const clothes = result[0].rows;
+    const variants = result[1].rows;
+
+    const clothesWithVariants = clothes.map((clothe) => {
+      const variantsOfClothe = variants.filter(
+        (variant) => variant.clothes_id === clothe.id
+      );
+      return { ...clothe, variants: variantsOfClothe };
+    });
+
+    res.status(200).json(clothesWithVariants);
   } catch (err) {
     res.status(500).json("no hay productos con ese nombre");
   }
