e6a44d9480b93dcb08ee15d8ae12605ee09f27a9
alexander fernandez <alex97l@hotmail.com>
2023-01-14 03:08:09 -0300

    footer links

--- components/utils/footer/FooterLink.js
+++ components/utils/footer/FooterLink.js
@@ -4,7 +4,10 @@ import React from "react";
 export const FooterLink = ({ slug = "", name = "" }) => {
   return (
     <li className="p-b-10">
-      <Link href={`/${slug}`} className="stext-107 cl7 hov-cl1 trans-04">
+      <Link
+        href={`/categorias/${slug}`}
+        className="stext-107 cl7 hov-cl1 trans-04"
+      >
         {name}
       </Link>
     </li>
