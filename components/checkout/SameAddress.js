86f0174c6a321c7aa8ed68d2fa7d37e34b11a94d
alexander fernandez <alex97l@hotmail.com>
2023-02-02 22:59:54 -0300

    checkmark

--- components/checkout/SameAddress.js
+++ components/checkout/SameAddress.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 export const SameAddress = ({ same, setSame }) => {
+  // need to add a function to set the same state to true or false when the checkbox is clicked and then equal the shipping address to the billing address
   return (
     <div className="form-check mt-2">
       <input
