f4e9495ff5911e9ac416e4aeb12b12844fed81fd
alexander fernandez <alex97l@hotmail.com>
2023-06-21 18:33:15 -0300

    refactor route - leftjoin

--- src/app/api/clothes/limit/route.ts
+++ src/app/api/clothes/limit/route.ts
@@ -5,29 +5,16 @@ import { eq, inArray } from "drizzle-orm";
 
 export async function GET(request: Request) {
   try {
-    console.log("request.headers" + request.headers);
     const key = request.headers.get("store-id") || "1";
-    const clothes_query = await db
+
+    const clothes_with_variants = await db
       .select()
       .from(clothes)
       .where(eq(clothes.storeId, parseInt(key)))
-      .limit(10);
+      .limit(10)
+      .leftJoin(variants, eq(clothes.id, variants.clothesId));
 
-    const clothes_ids: number[] = clothes_query.map((clothe) => clothe.id);
-
-    const variants_query = await db
-      .select()
-      .from(variants)
-      .where(inArray(variants.clothesId, clothes_ids));
-
-    const result = clothes_query.map((clothe) => {
-      const variant = variants_query.find(
-        (variant) => variant.clothesId === clothe.id
-      );
-      return { ...clothe, variants: variant };
-    });
-
-    return NextResponse.json(result);
+    return NextResponse.json(clothes_with_variants);
   } catch (error) {
     console.log(error);
     return NextResponse.json([]);
